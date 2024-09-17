5dd0675923483b24d6f9bca52938b77e7caa23bb
erimusumi <michaelsfrancek@gmail.com>
2024-09-17 10:20:11 -0400

    Outline for Mario interface

---
+++ ConcreteInterfaces/IMario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel-Plumbers-Fall-2024.ConcreteInterfaces
+{
+    interface IMario
+    {
+    void TakeDamage();
+    void Update();
+    void Jump();
+    void Move();
+    }
+}
