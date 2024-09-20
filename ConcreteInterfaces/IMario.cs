72cc35c16248aa7e3271488af3ebe41d54e793b9
erimusumi <michaelsfrancek@gmail.com>
2024-09-20 14:04:45 -0400

    Expanded IMario

--- ConcreteInterfaces/IMario.cs
+++ ConcreteInterfaces/IMario.cs
@@ -9,6 +9,50 @@ public interface IMario
     void TakeDamage();
     void Update();
     void Jump();
+    //Move method might be unneccessary, if there's a more appropriate place for it
     void Move();
+}
+
+class Mario : IMario
+{
+    IMarioState state;
+
+    public void TakeDamage()
+    {
+        state.TakeDamage();
+    }
+    public void Jump()
+    {
+        state.Jump();
+    }
+    public void Update()
+    {
+        state.Update();
+    }
+    public void Move()
+    {
+        
+        state.Move();
+    }
+}
+
+class StarMario : IMario
+{
+    public void TakeDamage()
+    {
+
+    }
+    public void Jump()
+    {
+
+    }
+    public void Update()
+    {
+
+    }
+    public void Move()
+    {
+
+    }
 }
 
