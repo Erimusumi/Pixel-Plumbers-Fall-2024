ab6fb62b464fa5967aff7eff0c5a6994143b08b9
erimusumi <michaelsfrancek@gmail.com>
2024-09-20 14:04:45 -0400

    Expanded IMario again

--- ConcreteInterfaces/IMario.cs
+++ ConcreteInterfaces/IMario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pixel_Plumbers_Fall_2024;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,133 @@ public interface IMario
     void TakeDamage();
     void Update();
     void Jump();
+    //Move method might be unneccessary, if there's a more appropriate place for it
     void Move();
+}
+
+class Mario : IMario
+{
+    IMarioState state;
+
+    public void TakeDamage()
+    {
+        state.TakeDamage();
+    }
+    public void Jump()
+    {
+        state.Jump();
+    }
+    public void Update()
+    {
+        state.Update();
+    }
+    public void Move()
+    { 
+        state.Move();
+    }
+}
+
+class StarMario : IMario
+{
+    IMario decoratedMario;
+    //Replace timer with however long the star lasts
+    int timer = 0;
+    
+    public StarMario (IMario decoratedMario)
+    {
+        this.decoratedMario = decoratedMario;
+    }
+
+    public void TakeDamage()
+    {
+        //Left empty; Mario is invincible
+    }
+    public void Jump()
+    {
+        decoratedMario.Jump();
+    }
+    public void Update()
+    {
+        timer--;
+        if (timer == 0)
+        {
+            RemoveStar();
+        }
+
+        decoratedMario.Update();
+    }
+
+    public void RemoveStar()
+    {
+        //Copied directly from carmen, change to better fix our project
+        Game1.Instance.mario = decoratedMario;
+    }
+    public void Move()
+    {
+        decoratedMario.Move();
+    }
+}
+class BigMario : IMario
+{
+    IMario decoratedMario;
+    public BigMario(IMario decoratedMario)
+    {
+        this.decoratedMario = decoratedMario;
+    }
+
+    public void TakeDamage()
+    {
+        decoratedMario.TakeDamage();
+    }
+    public void Jump()
+    {
+        decoratedMario.Jump();
+    }
+    public void Update()
+    {
+        decoratedMario.Update();
+    }
+
+    public void RemovePowerup()
+    {
+        //Copied directly from carmen, change to better fix our project
+        Game1.Instance.mario = decoratedMario;
+    }
+    public void Move()
+    {
+        decoratedMario.Move();
+    }
+}
+
+class FireMario : IMario
+{
+    IMario decoratedMario;
+    public FireMario(IMario decoratedMario)
+    {
+        this.decoratedMario = decoratedMario;
+    }
+
+    public void TakeDamage()
+    {
+        decoratedMario.TakeDamage();
+    }
+    public void Jump()
+    {
+        decoratedMario.Jump();
+    }
+    public void Update()
+    {
+        decoratedMario.Update();
+    }
+
+    public void RemovePowerup()
+    {
+        //Copied directly from carmen, change to better fix our project
+        Game1.Instance.mario = decoratedMario;
+    }
+    public void Move()
+    {
+        decoratedMario.Move();
+    }
 }
 
--- MarioClasses/BaseMario.cs
+++ MarioClasses/BaseMario.cs
@@ -4,25 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pixel-Plumbers-Fall-2024.MarioClasses
-{
-    class Mario : IMario
-    {
-    public void TakeDamage()
-    {
+//Moving everything here to IMario
 
-    }
-    public void Jump()
-    {
-
-    }
-    public void Update()
-    {
-
-    }
-    public void Move()
-    {
-
-    }
-    }
-}
+//namespace Pixel-Plumbers-Fall-2024.MarioClasses
+//{
+    
+//}
