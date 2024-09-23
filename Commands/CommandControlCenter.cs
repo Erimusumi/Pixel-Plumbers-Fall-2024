cb25c85fda2400c19efe083eea1a1eeaf092262c
Arun Ghimire <ghimire.36@osu.edu>
2024-09-23 14:11:20 -0400

    added command control center

---
+++ Commands/CommandControlCenter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Pixel_Plumbers_Fall_2024;
+using System.Globalization;
+public class CommandControlCenter
+{
+    private ICommand SetMovingRightMarioCommand;
+    private ICommand SetMovingLeftMarioCommand;
+    private ICommand SetJumpingUpMarioCommand;
+    private ICommand EnemySwitch;
+    private KeyboardController keyboardController;
+    public CommandControlCenter(Game1 game)
+	{
+        keyboardController.addCommand(Keys.Right, SetMovingRightMarioCommand);
+        keyboardController.addCommand(Keys.Left, SetMovingLeftMarioCommand);
+        keyboardController.addCommand(Keys.Up, SetJumpingUpMarioCommand);
+        keyboardController.addCommand(Keys.P, EnemySwitch);
+        keyboardController.addCommand(Keys.O, EnemySwitch);
+        game.SetKey(keyboardController);
+    }
+
+
+
+}
