273215b42ab3f4ad462c60436154cd920d912b6d
Arun Ghimire <ghimire.36@osu.edu>
2024-09-24 10:51:11 -0400

    Enemy Stuff

---
+++ Koopa/Koopa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Sprint0;
+
+public class Koopa : ISpriteEnemy
+{
+    private KoopaStateMachine stateMachine;
+	public Koopa()
+	{
+		stateMachine = new KoopaStateMachine();
+	}
+
+	public void changeDirection()
+	{
+		stateMachine.changeDirection();
+	}
+
+	public void beStomped()
+	{
+		stateMachine.beStomped();
+	}
+	public void beFlipped()
+	{
+		stateMachine.beFlipped();
+	}
+
+	public void Updates()
+	{
+		stateMachine.Update();
+	}
+
+	public void Draw(SpriteBatch sb, Texture2D Texture)
+	{
+        stateMachine.Draw(sb, Texture);
+    }
+
+}
