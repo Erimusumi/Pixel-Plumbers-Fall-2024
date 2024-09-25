ca93368b7f08a7e3fa6b089db9a51fd9033dc7a0
Sanaa Kamau <kamausanaa@gmail.com>
2024-09-25 13:33:13 -0400

    Slice FirePower

---
+++ Sprites/Item Sprites/FirePower.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class FirePower : ISprite
+{
+    private Texture2D ItemTexture;
+    public FirePower(Texture2D ItemTexture)
+    {
+        this.ItemTexture = ItemTexture;
+    }
+    public void Draw(SpriteBatch spriteBatch, Vector2 position)
+    {
+        Rectangle sourceRectangle = new Rectangle(287, 213, 65, 64);
+        spriteBatch.Draw(ItemTexture, position, sourceRectangle, Color.White);
+    }
+
+    public void Load(GraphicsDeviceManager graphics)
+    {
+    }
+
+    public void Update(GameTime gametime)
+    {
+    }
+}
--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257229f3b54f911acbfc8eea31131383cf82bf0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e15f7f7dc2793572ea6ddc9882fefd9e1fe9288")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
