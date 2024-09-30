8b1315098b50d18a70bd9721117554084f60155f
Arun Ghimire <ghimire.36@osu.edu>
2024-09-29 23:30:14 -0400

    Switch Mario State / Movement

---
+++ Commands/otherCommands/resetCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Pixel_Plumbers_Fall_2024;
+
+public class resetCommand : ICommand
+{
+	Game1 g;
+	public resetCommand(Game1 game)
+	{
+		g = game;
+	}
+
+	public void Execute()
+	{
+		
+	}
+}
