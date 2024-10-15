f67db4509cbcf2ca48f3a3f123facc69b7fd9c78
Dylan Moore <moore.4799@buckeyemail.osu.edu>
2024-10-15 14:37:33 -0400

    Other enemy interactions

---
+++ Collision/Interaction/OtherEnemyInteractions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Pixel_Plumbers_Fall_2024;
+
+public class OtherEnemyInteraction
+{
+    ISpriteEnemy enemy;
+    Mario mario;
+    public OtherEnemyInteraction(ISpriteEnemy _enemy)
+    {
+        enemy = _enemy;
+    }
+    public void update()
+    {
+        enemy.changeDirection();
+    }
+}
+
--- Collision/Sweep.cs
+++ Collision/Sweep.cs
@@ -12,6 +12,9 @@ public class Sweep
 {
     public enum CollisionType { Top, Bottom, Left, Right, DontCare };
 
+
+    EnemyMarioInteraction EnemyMarioInteraction;
+    OtherEnemyInteraction OtherEnemyInteraction;
     //Pass some list
     //Sweep should 
     public void handleInteraction(List<IEntity> entities, int index1, int index2)
@@ -20,10 +23,18 @@ public class Sweep
         /*Should determine the interactionType of two entities and call the appropriate method */
         IEntity item1 = entities[index1];
         IEntity item2 = entities[index2];
-        if (item1.GetType() == typeof(ISpriteEnemy) && item2.GetType() == typeof(Mario) || item1.GetType() == typeof(Mario) && item2.GetType() == typeof(ISpriteEnemy))
+        if (item1.GetType() == typeof(ISpriteEnemy) && item2.GetType() == typeof(Mario))
         {
-            //handle enemy interaction
-            
+            EnemyMarioInteraction = new EnemyMarioInteraction((ISpriteEnemy)item1, (Mario)item2, item1.GetDestination(), item2.GetDestination());
+        } else if (item1.GetType() == typeof(Mario) && item2.GetType() == typeof(ISpriteEnemy))
+        {
+            EnemyMarioInteraction = new EnemyMarioInteraction((ISpriteEnemy)item2, (Mario)item1, item2.GetDestination(), item1.GetDestination());
+        } else if (item1.GetType() == typeof(ISpriteEnemy))
+        {
+            OtherEnemyInteraction = new OtherEnemyInteraction((ISpriteEnemy)item1);
+        } else if (item2.GetType() == typeof(ISpriteEnemy))
+        {
+            OtherEnemyInteraction = new OtherEnemyInteraction((ISpriteEnemy)item2);
         }
         //Shouldn't need different methods for different types of enemies,
         //keep here just in case though
