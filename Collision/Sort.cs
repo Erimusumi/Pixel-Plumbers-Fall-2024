968e18de2cd21aa521ff1a47985a31f178ea7088
Dylan Moore <moore.4799@buckeyemail.osu.edu>
2024-10-14 20:51:43 -0400

    Changed SortList

--- Collision/Sort.cs
+++ Collision/Sort.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Pixel_Plumbers_Fall_2024;
 
 public class Sort
 {
+    int cameraX, cy, cw, ch;
+    Rectangle start;
+    Rectangle next;
+    int holdElementNumber = 0;
+    int entered = 0;
+    int startX = 0, sy, sw, sh;
+    int nextX = 0, ny, nw, nh;
+    int smallestX = 0;
     public void Add(List<IEntity> objects, IEntity newObject)
     {
         objects.Add(newObject);
     }
 
-    public void Remove(List<Object> objects, Object objToRemove)
+    public void Remove(List<IEntity> objects, IEntity objToRemove)
     {
         if (!objects.Remove(objToRemove))
         {
@@ -22,10 +31,38 @@ public class Sort
             Environment.Exit(50);
         }
     }
-
-    public void SortList(List<Object> objects)
+    //Assume the list has at least 2 elements, besides a floor at [0]
+    //[0, 30, 10, 20]
+    //[0, 20, 10, 30]
+    //[0, 30, 20, 10]
+    //[0, 20, 30, 10]
+    public void SortList(List<IEntity> objects, Rectangle camera)
     {
-        
+        camera.Deconstruct(out cameraX, out cy, out cw, out ch);
+        for (int i = 1; (i < objects.Count) && (startX < cameraX); i++)
+        {
+            start = objects[i].GetDestination();
+            for(int j = 2; (j < objects.Count) && (i != j) && (nextX < cameraX); j++)
+            {
+                next = objects[j].GetDestination();
+                start.Deconstruct(out startX, out sy, out sw, out sh);
+                next.Deconstruct(out nextX, out ny, out nw, out nh);
+                if ((nextX < startX) && (nextX < smallestX))
+                {
+                    smallestX = nextX;
+                    holdElementNumber = j;
+                    entered++;
+                }
+            }
+            if (entered > 0)
+            {
+                IEntity holdE = objects[holdElementNumber];
+                objects[holdElementNumber] = objects[i];
+                objects[i] = holdE;
+            }
+            entered = 0;
+
+        }
 
     }
     public void clearList(List<Object> objects)
