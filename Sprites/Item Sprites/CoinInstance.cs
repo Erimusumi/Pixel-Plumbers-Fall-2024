f92b5b057b7ff4a61ebd1a125b5ca7f797205640
Sanaa Kamau <kamausanaa@gmail.com>
2024-10-17 14:30:00 -0400

    coin animation

--- Sprites/Item Sprites/CoinInstance.cs
+++ Sprites/Item Sprites/CoinInstance.cs
@@ -5,6 +5,7 @@ public class CoinInstance : ISprite
 {
     private Texture2D ItemTexture;
     private Rectangle[] frames;
+    private int[] frameWidths;
     private int currentFrame;
     private int lastFrame;
     //private Vector2 position;
@@ -12,26 +13,35 @@ public class CoinInstance : ISprite
     public CoinInstance(Texture2D ItemTexture)
     {
         this.ItemTexture = ItemTexture;
+
         frames = new Rectangle[5];
         frames[0] = new Rectangle(2, 81, 10, 15);
         frames[1] = new Rectangle(4, 96, 5, 13);
         frames[2] = new Rectangle(18, 96, 8, 15);
         frames[3] = new Rectangle(39, 96, 2, 15);
         frames[4] = new Rectangle(55, 96, 1, 15);
+
+        frameWidths[0] = 10;
+        frameWidths[1] = 5;
+        frameWidths[2] = 8;
+        frameWidths[3] = 2;
+        frameWidths[4] = 1;
+
         currentFrame = 0;
         lastFrame = 4;
     }
     public void Draw(SpriteBatch spriteBatch, Vector2 position)
     {
-        Rectangle sourceRectangle = new Rectangle(0, 33, 15, 15);
+        Rectangle sourceRectangle = frames[currentFrame];
         spriteBatch.Draw(ItemTexture, position, sourceRectangle, Color.White);
     }
 
     public void Update(GameTime gametime)
     {
+
     }
     public Rectangle GetDestination(Vector2 position)
     {
-        return new Rectangle((int)position.X, (int)position.Y, 15, 15);
+        return new Rectangle((int)position.X, (int)position.Y, frameWidths[currentFrame], 15);
     }
 }
