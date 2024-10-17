effeb74eb54d48cf4b776551b936a3c92470153c
Sanaa Kamau <kamausanaa@gmail.com>
2024-10-17 14:23:35 -0400

    coin update

--- Sprites/Item Sprites/CoinInstance.cs
+++ Sprites/Item Sprites/CoinInstance.cs
@@ -4,11 +4,22 @@ using Microsoft.Xna.Framework.Graphics;
 public class CoinInstance : ISprite
 {
     private Texture2D ItemTexture;
+    private Rectangle[] frames;
+    private int currentFrame;
+    private int lastFrame;
     //private Vector2 position;
 
     public CoinInstance(Texture2D ItemTexture)
     {
         this.ItemTexture = ItemTexture;
+        frames = new Rectangle[5];
+        frames[0] = new Rectangle(2, 81, 10, 15);
+        frames[1] = new Rectangle(4, 96, 5, 13);
+        frames[2] = new Rectangle(18, 96, 8, 15);
+        frames[3] = new Rectangle(39, 96, 2, 15);
+        frames[4] = new Rectangle(55, 96, 1, 15);
+        currentFrame = 0;
+        lastFrame = 4;
     }
     public void Draw(SpriteBatch spriteBatch, Vector2 position)
     {
