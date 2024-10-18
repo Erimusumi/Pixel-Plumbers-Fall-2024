6da46b5db99e6b3d90a5f91b694afdff801fc80e
Arun Ghimire <ghimire.36@osu.edu>
2024-10-18 11:48:05 -0400

    Mario Class Update

--- GameStateMachine.cs
+++ GameStateMachine.cs
@@ -14,6 +14,7 @@ public class GameStateMachine
     {
         currentGameState = GameStates.Running;
     }
+    
     public void setGameStatePaused()
     {
         currentGameState = GameStates.Paused;
