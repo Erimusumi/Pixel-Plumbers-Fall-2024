c679e0dc453cf9025f60596d068a1f169edfa140
erimusumi <michaelsfrancek@gmail.com>
2024-10-21 19:50:38 -0400

    fireballs bounce on ground

--- Collision/Interaction/BlockFireballInteraction.cs
+++ Collision/Interaction/BlockFireballInteraction.cs
@@ -10,16 +10,15 @@ using System.Threading.Tasks;
     public class BlockFireballInteraction
 {
     private Fireball _fireball;
-    private UnknownBlockSprite _block;
+    private IBlock _block;
     private List<IEntity> _entities;
     private List<IEntity> _entitiesRemoved;
 
-    public BlockFireballInteraction(Fireball fireball, UnknownBlockSprite block, List<IEntity> entities)
+    public BlockFireballInteraction(Fireball fireball, IBlock block, List<IEntity> entitiesRemoved)
     {
         this._block = block;
         this._fireball = fireball;
-        this._entities = entities;
-        //this._entitiesRemoved = entitiesRemoved;
+        this._entitiesRemoved = entitiesRemoved;
     }
     public void update()
     {
@@ -28,11 +27,7 @@ using System.Threading.Tasks;
         Rectangle temp = Rectangle.Intersect(_block.GetDestination(), _fireball.GetDestination());
         if (temp.Width > temp.Height)
         {
-            //Check if fireball is on top or bottom
-            if (_fireball.GetDestination().Center.Y > _block.GetDestination().Center.Y)
-            {
-                _fireball.Bounce();
-            }
+            _fireball.Bounce();
         }
         else
         {
@@ -42,7 +37,7 @@ using System.Threading.Tasks;
     }
     private void removeFromList()
     {
-        //_entitiesRemoved.Add(_fireball);
+        _entitiesRemoved.Add(_fireball);
         _fireball.Remove();
     }
 }
