20c773893382053ca71a4f6a79dddb400ff03bc9
Sanaa Kamau <kamausanaa@gmail.com>
2024-10-21 18:26:38 -0400

    sweep obstacle collision

--- Collision/Sweep.cs
+++ Collision/Sweep.cs
@@ -174,8 +174,8 @@ public class Sweep
         }
         if(ContainsObstacle(entities,index1) && ContainsItem(entities, index2))
         {
-            //ItemObstacleInteraction = new ItemObstacleInteraction((Mushroom)entities[index2], (IObstacle)entities[index2]);
-           // ItemObstacleInteraction.update();
+            ItemObstacleInteraction = new ItemObstacleInteraction((IItem)item2, (IObstacle)item1);
+           ItemObstacleInteraction.update();
         }else if(ContainsObstacle(entities,index2) && ContainsItem(entities, index1)){
 
         }
--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1109702f34d0efe102b62417e86638b3f9896521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbfba8d3c0c9ee3e76cc9cf38fc206ea8717b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
