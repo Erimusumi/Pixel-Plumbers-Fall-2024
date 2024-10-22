0bc9078402ee67335ba32d59d5e5dafccc9b2637
Helen Wang <helenwang162@gmail.com>
2024-10-21 22:36:21 -0400

    .

--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f155dddf6f1e56c49cd4ef141dc38d16600194a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4691f2b4cbc998326ed10c5a2833a1b7514b0c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
