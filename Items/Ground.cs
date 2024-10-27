33db12258e4a61348ed1e3e18beb9704afcddd63
Sanaa Kamau <kamausanaa@gmail.com>
2024-10-27 12:26:09 -0400

    Ground Object

---
+++ Items/Ground.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Ground: IEntity
+{
+    private Vector2 position;
+    private Texture2D texture;
+    private SpriteBatch sB;
+    private GroundTile g;
+    public Ground(Vector2 pos, Texture2D texture, SpriteBatch sB)
+    {
+        g = new GroundTile(texture);
+        this.position = pos;
+        this.texture = texture;
+
+    }
+    public void draw()
+    {
+        g.Draw( sB, position);
+    }
+    public void Update()
+    {
+
+    }
+    
+   public Rectangle GetDestination()
+    {
+        return this.g.GetDestination(position);
+    }
+
+}
+
---
+++ Sprites/Item Sprites/GroundTile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+   public class GroundTile
+{
+    private Texture2D ItemTexture;
+    private int width;
+        private int  height;
+    //private Vector2 position;
+
+    public GroundTile(Texture2D ItemTexture)
+    {
+        this.ItemTexture = ItemTexture;
+      
+    }
+    public void Draw(SpriteBatch spriteBatch, Vector2 position)
+    {
+        Rectangle sourceRectangle = new Rectangle(0, 0, 0, 0); //TODO: Get source rectangle
+
+        spriteBatch.Draw(ItemTexture, position, sourceRectangle, Color.White);
+
+    }
+
+    public void Update(GameTime gametime)
+    {
+    }
+    public Rectangle GetDestination(Vector2 position)
+    {
+        return new Rectangle((int)position.X, (int)position.Y, width, height);
+    }
+}
+
+
