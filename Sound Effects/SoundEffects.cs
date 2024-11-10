53082ff8d44fc66635f4f3b1ccc727faf4491f0e
Sanaa Kamau <kamausanaa@gmail.com>
2024-11-10 13:14:11 -0500

    .

--- Sound Effects/SoundEffects.cs
+++ Sound Effects/SoundEffects.cs
@@ -7,5 +7,23 @@ using System.Threading.Tasks;
 
     public class SoundEffects
 {
+
+    public void LoadSoundEffects()
+    {
+
+    }
+    public void playBrokenBlock()
+    {
+
+    }
+    public void playCoinCollect()
+    {
+
+    }
+    public void playFireBall()
+    {
+
+    }
+    //TODO: add other sound effect methods
 }
 
