db5292259e538eade618ef3d1b9ad1abde8747de
Sanaa Kamau <kamausanaa@gmail.com>
2024-11-18 18:15:14 -0500

    .

--- Levels/LevelThree.cs
+++ Levels/LevelThree.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Pixel_Plumbers_Fall_2024;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,20 @@ using System.Threading.Tasks;
 
 public class LevelThree:ILevel
 {
+    private Game1 game;
+    private Rectangle screen = new Rectangle(0, 0, 800, 480);
+    private List<IEntity> entities = new List<IEntity>();
+    private List<IBlock> blocks;
+    private Mario mario;
+    GameTime gameTime;
+    List<IEntity> entitiesRemoved;
+
+    private SpriteBatch spriteBatch;
+
+    private Texture2D EnemyTexture;
+    private Texture2D blockTexture;
+    private Texture2D obstacleTexture;
+    private Texture2D ItemsTexture;
     public void LoadLevel(ContentManager content)
     {
 
--- Levels/LevelTwo.cs
+++ Levels/LevelTwo.cs
@@ -6,10 +6,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pixel_Plumbers_Fall_2024;
 
 
 public class LevelTwo : ILevel
+
+
 {
+    private Game1 game;
+    private Rectangle screen = new Rectangle(0, 0, 800, 480);
+    private List<IEntity> entities = new List<IEntity>();
+    private List<IBlock> blocks;
+    private Mario mario;
+    GameTime gameTime;
+    List<IEntity> entitiesRemoved;
+
+    private SpriteBatch spriteBatch;
+
+    private Texture2D EnemyTexture;
+    private Texture2D blockTexture;
+    private Texture2D obstacleTexture;
+    private Texture2D ItemsTexture;
     public void LoadLevel(ContentManager content)
     {
 
