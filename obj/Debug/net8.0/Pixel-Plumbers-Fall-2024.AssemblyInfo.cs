69a1ba73e2f5e72ccd6ebe1119ece2a4c3026e3b
Sanaa Kamau <kamausanaa@gmail.com>
2024-11-18 17:16:56 -0500

    fixed issue of mario falling into the ground

--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b530c2411e8c103f6f904f740b4e4aaefaf32a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfebf4cde9bded03126fc6f42d06d3a5b499b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
