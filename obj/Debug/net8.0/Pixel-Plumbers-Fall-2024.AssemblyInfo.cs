a56e578e8ed77c851871d33a59a64ced3c167e67
erimusumi <michaelsfrancek@gmail.com>
2024-11-24 17:46:17 -0500

    .

--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a148e0b6213f826f89d355e8e48b2c0cc07caa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0a9a6767e8074d1756a7b29cba15bc656ab1da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
