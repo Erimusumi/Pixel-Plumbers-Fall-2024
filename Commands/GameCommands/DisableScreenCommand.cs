dabb5504228fa04d43ac2a647bb9a7ff04f8bc9f
Dylan Moore <moore.4799@buckeyemail.osu.edu>
2024-12-04 22:41:36 -0500

    .

--- Commands/GameCommands/DisableScreenCommand.cs
+++ Commands/GameCommands/DisableScreenCommand.cs
@@ -1,11 +1,23 @@
 ﻿using Pixel_Plumbers_Fall_2024;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 public class DisableScreenCommand : ICommand
 {
-    public DisableScreenCommand()
+    Dictionary<Rectangle, ICommand> list;
+    MouseController gameMouseController;
+    public DisableScreenCommand(Dictionary<Rectangle, ICommand> list, MouseController gameMouseController, Rectangle levelRec)
     {
+        this.list = list;
+        this.gameMouseController = gameMouseController;
+        gameMouseController.RemoveCommand(levelRec);
     }
     public void Execute()
     {
+        foreach (var item in list)
+        {
+            gameMouseController.RemoveCommand(item.Key);
+        }
     }
 }
--- Commands/GameCommands/LevelTwoCommands.cs
+++ Commands/GameCommands/LevelTwoCommands.cs
@@ -1,11 +1,14 @@
 using Pixel_Plumbers_Fall_2024;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 public class LevelTwoCommand : ICommand
 {
     private GameStateMachine gameStateMachine;
-    public LevelTwoCommand(GameStateMachine gameStateMachine)
+    public LevelTwoCommand(GameStateMachine gameStateMachine, Rectangle thisRectangle, Dictionary<Rectangle, ICommand> list, MouseController gameMouseController)
     {
         this.gameStateMachine = gameStateMachine;
+
     }
     public void Execute()
     {
