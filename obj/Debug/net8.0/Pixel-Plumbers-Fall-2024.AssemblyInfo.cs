5e845e88c9aeb28d0a0f3690a3e51d38a9256512
Sanaa Kamau <kamausanaa@gmail.com>
2024-12-10 12:08:52 -0500

    .

--- obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pixel-Plumbers-Fall-2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d369d2b66c7830f160581ff2abf81aeb389d1a39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e811e8e1e1dc1910ac8ef5a83963a4b36444ccd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixel-Plumbers-Fall-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
